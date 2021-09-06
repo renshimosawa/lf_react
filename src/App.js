a7f5b46b771bafc540a2564990362ac31598733e
renshimosawa <polaris2760@gmail.com>
2021-09-06 23:56:00 +0900

    materialUI 追加した

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 const Sample = {
   foo: {num: 2}, 
   bar: {num: -1}, 
@@ -22,10 +26,24 @@ const fooSum = Sample2.map((item) => item.foo).filter(v => v).reduce((sum,num) =
 const barSum = Sample2.map((item) => item.bar).filter(v => v).reduce((sum,num) => sum + num) 
 console.log({'foo' : fooSum, 'bar' : barSum});
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '90vw',
+    },
+  },
+}));
+
 const App= () => {
+  const classes = useStyles();  
   return (
     <div>
-      aaa
+      {/* <img src="https://images.unsplash.com/photo-1624313976899-0fd4989a2fcd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"></img> */}
+      <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="filled-basic" label="URL" variant="filled" />
+</form>
     </div>
   )
 }
